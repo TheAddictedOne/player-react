21a3316e11f68b6d10e48835b9bd651c21b11fd3
Virac David AUK <auk.david@gmail.com>
2021-11-09 10:18:24 +0100

    Add traking log

--- public/tracking.js
+++ public/tracking.js
@@ -1,17 +1,21 @@
 (function() {
   // Declarations
+  const CATEGORY_VITALS = 'vitals'
   const IFRAME_REQUESTED = 'iframe_requested'
+  const IFRAME_STARTS = 'iframe_starts'
 
   let lastMark = IFRAME_REQUESTED
 
   function vitals(name) {
-    performance.mark(name)
+    const mark = performance.mark(name)
+    mark.category = CATEGORY_VITALS
+    console.log(mark)
   }
 
   function observerCallback(list) {
     list.getEntries().forEach((entry) => {
       performance.measure(
-        `${lastMark}-to-${entry.name}`,
+        `${lastMark} to ${entry.name}`,
         lastMark === IFRAME_REQUESTED ? undefined : lastMark,
         entry.name
       )
@@ -19,10 +23,20 @@
     })
   }
 
+  function log() {
+    const table = []
+    performance.getEntries().forEach(({ category, name, startTime }) => {
+      if (category !== CATEGORY_VITALS) return
+
+      table.push({ name, startTime })
+    })
+    console.table(table)
+  }
+
   // Execution
   new PerformanceObserver(observerCallback).observe({ type: 'mark' })
-  vitals('iframe_starts')
+  vitals(IFRAME_STARTS)
 
   // Export
-  window.tracking = { vitals }
+  window.tracking = { vitals, log }
 })()
